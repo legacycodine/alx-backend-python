7dc49a6457d4f744dcd11f932bc367f98bff5927
legacycodine <adebimpee842@gmail.com>
2023-08-02 19:06:50 +0000

    python variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""add takes a float a and float b as argument"""
+
+
+def add(a: float, b: float) -> float:
+    """type-annotated function add takes float a n float b
+       as arguments and returns their sum as a float
+    """
+    return a + b
